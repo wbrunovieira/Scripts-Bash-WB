886160d19b6a802c40684c7b5ba3b58fa777885a
wbrunovieira <wbrunovieira77@gmail.com>
2023-12-07 20:44:56 -0300

    another try

--- comitar.sh
+++ comitar.sh
@@ -2,7 +2,7 @@
 
 read -p "Digite a mensagem de commit: " commit_message
 
-git add . >/dev/null 2>&1
+git_add_output=$(git add . 2>&1)
 
 status=$(git status --porcelain)
 
@@ -12,6 +12,6 @@ if [[ -z $status ]]; then
     echo "Bruno, nao tem nada para enviar. Presta atencao :-)"
 else
     
-    git commit -m "$commit_message"
-    git push -u origin main
+    git_commit_output=$(git commit -m "$commit_message" 2>&1)
+    git_push_output=$(git push -u origin main 2>&1)
 fi
