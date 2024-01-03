f1be8488322364650903ceee29cd4c07f4f1b179
wbrunovieira <wbrunovieira77@gmail.com>
2024-01-02 22:47:15 -0300

    added script to run every day, schedule in the cron

---
+++ verifica_espaco.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Obtém a saída do comando df -h e processa linha por linha
+df -h | while read line; do
+    if [[ $line == /dev/* ]]; then
+        # Extrai os campos de interesse
+        filesystem=$(echo $line | awk '{print $1}')
+        size=$(echo $line | awk '{print $2}')
+        used=$(echo $line | awk '{print $3}')
+        available=$(echo $line | awk '{print $4}')
+        capacity=$(echo $line | awk '{print $5}')
+        
+        # Calcula a porcentagem livre
+        percent_free=$(echo $capacity | tr -d '%')
+        percent_free=$((100 - percent_free))
+
+        # Formata e exibe a mensagem
+        echo "Filesystem: $filesystem, Tamanho Total: $size, Usado: $used, Disponível: $available, Capacidade Usada: $capacity, Espaço Livre: $percent_free%"
+    fi
+done
+
